344a85c5a31539aa6a267d7aa714a73e6231dad0
OvHvO <waikitpua1128@gmail.com>
2025-04-01 09:11:34 +0800

    Sales Manager UI design

--- src/main/java/com/salesmanager/UI/smUICtrl.java
+++ src/main/java/com/salesmanager/UI/smUICtrl.java
@@ -1,5 +1,6 @@
 package com.salesmanager.UI;
 
+import javafx.animation.FadeTransition;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -32,24 +33,75 @@ public class smUICtrl implements Initializable {
 		loadNewContent("/fxml/smSuppliers.fxml");
 	}
 	
-	public void loadNewContent(String fxmlFile) {
-		
-        try {
-        	
-            AnchorPane newContent = FXMLLoader.load(getClass().getResource(fxmlFile));
-
-            contentPane.getChildren().setAll(newContent);
-
-            AnchorPane.setTopAnchor(newContent, 0.0);
-            AnchorPane.setBottomAnchor(newContent, 0.0);
-            AnchorPane.setLeftAnchor(newContent, 0.0);
-            AnchorPane.setRightAnchor(newContent, 0.0);
-            
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+	@FXML
+	private void handleChangeDailyS() {
+		loadNewContent("/fxml/smDailySales.fxml");
 	}
 	
+	@FXML
+	private void handleChangePO() {
+		loadNewContent("/fxml/smPO.fxml");
+	}
+	
+	@FXML
+	private void handleChangePR() {
+		loadNewContent("/fxml/smPR.fxml");
+	}
+	
+	public void loadNewContent(String fxmlFile) {
+		
+        FadeTransition fadeOut = new FadeTransition(Duration.seconds(1), contentPane);
+        fadeOut.setFromValue(1.0);
+       	fadeOut.setToValue(0.0); 
+       	fadeOut.setCycleCount(1);
+
+       	fadeOut.setOnFinished(event -> {
+    	 try {
+        	     AnchorPane newContent = FXMLLoader.load(getClass().getResource(fxmlFile));
+        	     contentPane.getChildren().setAll(newContent);
+
+        	      AnchorPane.setTopAnchor(newContent, 0.0);
+        	      AnchorPane.setBottomAnchor(newContent, 0.0);
+        	      AnchorPane.setLeftAnchor(newContent, 0.0);
+        	      AnchorPane.setRightAnchor(newContent, 0.0);
+
+        	      FadeTransition fadeIn = new FadeTransition(Duration.seconds(1), contentPane);
+        	      fadeIn.setFromValue(0.0);
+        	      fadeIn.setToValue(1.0);
+        	      fadeIn.setCycleCount(1);
+        	      fadeIn.play();
+        	} catch (IOException e) {
+        	        e.printStackTrace();
+        	}
+        });
+        fadeOut.play();
+	}
+	
+//  << Lambda Explanation>>
+//	fadeOut.setOnFinished(new EventHandler<ActionEvent>() {
+//	    @Override
+//	    public void handle(ActionEvent event) {
+//	        try {
+//
+//	            AnchorPane newContent = FXMLLoader.load(getClass().getResource(fxmlFile));
+//	            contentPane.getChildren().setAll(newContent);
+//
+//	            AnchorPane.setTopAnchor(newContent, 0.0);
+//	            AnchorPane.setBottomAnchor(newContent, 0.0);
+//	            AnchorPane.setLeftAnchor(newContent, 0.0);
+//	            AnchorPane.setRightAnchor(newContent, 0.0);
+//
+//	            FadeTransition fadeIn = new FadeTransition(Duration.seconds(1.5), contentPane);
+//	            fadeIn.setFromValue(0.0);
+//	            fadeIn.setToValue(1.0);
+//	            fadeIn.setCycleCount(1);
+//	            fadeIn.play();
+//	        } catch (IOException e) {
+//	            e.printStackTrace();
+//	        }
+//	    }
+//	});
+
 	@FXML
     private AnchorPane drawerPane;
 
