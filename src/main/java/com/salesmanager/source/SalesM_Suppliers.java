6a23fa5da86dc6b4c6d72998da213a2d9ceb10be
OvHvO <waikitpua1128@gmail.com>
2025-04-02 23:25:06 +0800

    lol

--- src/main/java/com/salesmanager/source/SalesM_Suppliers.java
+++ src/main/java/com/salesmanager/source/SalesM_Suppliers.java
@@ -1,5 +1,53 @@
 package com.salesmanager.source;
 
-public class SalesM_Suppliers {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+import com.groupfx.JavaFXApp.modifyData;
+import com.groupfx.JavaFXApp.viewData;
+
+public class SalesM_Suppliers  extends SalesM implements viewData, modifyData{
+	
+	@Override
+	public StringBuilder ReadTextFile() throws IOException
+	{	
+		//InputStream stream= getClass().getClassLoader().getResourceAsStream("Data/ItemsList.txt");
+		BufferedReader reader= new BufferedReader(new FileReader("Data/ItemsList.txt"));
+		builder= new StringBuilder();
+		String line;
+		
+		while ((line=reader.readLine())!=null) 
+		{
+			String[] data=line.split(",");
+			builder.append(data[0]).append(","); //ID
+			builder.append(data[1]).append(","); //Name
+			builder.append(data[2]).append(","); //Supplier Name
+			builder.append(data[3]).append(","); //Stock
+			builder.append(data[4]).append("\n"); //UnitPrice
+			
+		}
+		return builder;
+		
+	}
+	
+	@Override
+	public void AddFunc() {
+		
+	}
+	
+	@Override
+	public void EditFunc() {
+		
+	}
+	
+	@Override
+	public void DeleteFunc() {
+		
+	}
+	
+	@Override
+	public void SaveFunc() {
+		
+	}
 }
