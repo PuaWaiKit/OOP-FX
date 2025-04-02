6db6b86576f075d8590196ae79a3918838d16f91
OvHvO <waikitpua1128@gmail.com>
2025-04-02 11:12:41 +0800

    pr

---
+++ src/main/java/com/salesmanager/source/SalesM_PRs.java
@@ -0,0 +1,51 @@
+package com.salesmanager.source;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import com.groupfx.JavaFXApp.prSource;
+
+public class SalesM_PRs  extends SalesM implements prSource {
+	
+	@Override
+	public void viewPR() {
+		try {
+            StringBuilder data = ReadTextFile();
+            System.out.println(data.toString()); 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+
+	@Override
+	public void modifyPR() {
+		
+	}
+	
+
+	public StringBuilder ReadTextFile() throws IOException
+	{	
+		
+		InputStream stream= getClass().getClassLoader().getResourceAsStream("Data/prList.txt");
+		BufferedReader reader= new BufferedReader(new InputStreamReader(stream));
+		builder= new StringBuilder();
+		String line;
+		
+		while ((line=reader.readLine())!=null) 
+		{
+			String[] data=line.split(",");
+			builder.append(data[0]).append(","); //ID
+			builder.append(data[1]).append(","); //Name
+			builder.append(data[2]).append(","); //Supplier Name
+			builder.append(data[3]).append(","); //Stock
+			builder.append(data[4]).append(","); //UnitPrice
+			builder.append(data[5]).append("\n"); //Sales Manager
+			
+		}
+		
+		return builder;
+		
+	}
+}
